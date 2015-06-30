68bba6a2d4fd6c56aee5b76a3bd8cadbd9c942e3
WY08271 <wywy08271@outlook.com>
2015-07-01 00:09:09 +0800

    practice1.1-4
    practice1.1-4

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,3 +1,16 @@
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+var result = [];
+  for(var i = 0 ; i < collection_a.length ; i ++)
+  {
+  	for(var k = 0 ; k < collection_b.length ; k++)
+  	{
+  		if (collection_b[k] == collection_a[i])
+  		{
+  			result.push(collection_a[i]); 
+  		}
+  	}
+  }
+  console.log(result);
+  return result;
 }
+
--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,3 +1,17 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  var result = [];
+  var complete = object_b.value ;
+  for(var i = 0 ; i < collection_a.length ; i ++)
+  {
+  	for(var k = 0 ; k < complete.length ; k++)
+  	{
+  		if (complete[k] == collection_a[i])
+  		{
+  			result.push(collection_a[i]); 
+  		}
+  	}
+  }
+  console.log(result);
+  return result;
 }
