46a0695bbcf76f8328c15f74b619fa7289f8b792
WY08271 <wywy08271@outlook.com>
2015-07-18 10:22:40 +0800

    pro-pos
    refactoring

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,3 +1,36 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  var result =[];
+  var count = 1;
+for(var i = 0; i < collection.length; i++)
+{
+    if(collection[i].length < 2)
+    {
+    for(var j = i + 1; j < collection.length; j++)
+    {
+      if(collection[i] == collection[j])
+      {
+        count++;
+        collection.splice(j,1);
+        j--;
+      }
+    }
+    result.push({key:collection[i],count:count});
+    count = 1;
+    }
+    else
+    {  
+      var string = collection[i];
+      for(var i = 0; i < string.length; i ++)
+      {
+        if(string[i] == 5)
+        {
+          result.push({key:collection[i],count:string[i]});
+        }
+      } 
+    }
 }
+  return result;
+}
+//function conuntOne(){
+
+//}
